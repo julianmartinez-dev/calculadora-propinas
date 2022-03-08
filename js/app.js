6d81b7d7d51e661d9375c565a92d19a450a9591f
Julian Martinez <julian.fmartinez@gmail.com>
2022-03-08 11:54:47 -0300

    Modal para ingresar nuevo cliente listo

--- js/app.js
+++ js/app.js
@@ -1,5 +1,53 @@
 let cliente = {
-    mesa: '',
-    hora: '',
-    pedido: []
+  mesa: '',
+  hora: '',
+  pedido: [],
+};
+
+const btnGuardarCliente = document.querySelector('#guardar-cliente');
+btnGuardarCliente.addEventListener('click', guardarCliente);
+
+function guardarCliente() {
+  const mesa = document.querySelector('#mesa').value;
+  const hora = document.querySelector('#hora').value;
+
+  //Revisa si hay campos vacios
+  const camposVacios = [mesa, hora].some((e) => e === '');
+
+  if (camposVacios) {
+    //Verificar si ya hay una alerta
+    const existeAlerta = document.querySelector('.invalid-feedback');
+
+    if (!existeAlerta) {
+      const alerta = document.createElement('DIV');
+      alerta.classList.add('invalid-feedback', 'd-block', 'text-center');
+      alerta.textContent = 'Todos los campos son obligatorios';
+      document.querySelector('.modal-body form').appendChild(alerta);
+
+      setTimeout(() => {
+        alerta.remove();
+      }, 3000);
+    }
+
+    return;
+  }
+
+  //Asignar datos del formulario al cliente
+  cliente = {...cliente, mesa, hora}
+
+  //Ocultar el modal
+  const modalFormulario = document.querySelector('#formulario');
+  const modalBootstrap = bootstrap.Modal.getInstance(modalFormulario);
+  modalBootstrap.hide();
+
+  //Mostrar las secciones
+  mostrarSecciones()
+
+}
+
+function mostrarSecciones(){
+    const seccionesOcultas = document.querySelectorAll('.d-none');
+    seccionesOcultas.forEach(seccion => {
+        seccion.classList.remove('d-none')
+    });
 }
